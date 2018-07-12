736a3cb527fc3c71fc762f6e946aad82e3d387f5
William L <w-lindvall@outlook.com>
2018-07-12 17:15:06 +1000

    🚔 configure eslint & minor refactor

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const figlet = require('figlet');
 
 const files = require('./lib/files');
 const inquirer = require('./lib/inquirer');
-const {camelize} = require('./lib/utils');
-const {getMeyerReset, getMiniReset, getNormalize} = require('./lib/css');
+const { camelize } = require('./lib/utils');
+const { getMeyerReset, getMiniReset, getNormalize } = require('./lib/css');
 
 clear();
 
 console.log(
   chalk.yellow(
-    figlet.textSync('css reset', { horizontalLayout: 'half' })
-  )
+    figlet.textSync('css reset', { horizontalLayout: 'half' }),
+  ),
 );
 
 if (files.exists('reset.css')) {
@@ -23,24 +23,25 @@ if (files.exists('reset.css')) {
 }
 
 const run = async () => {
-  const {baseCSS} = await inquirer.askWhichCSSBase();
+  const { baseCSS } = await inquirer.askWhichCSSBase();
   const options = await inquirer.askForOptions(baseCSS);
-  userOptions = {};
-  options.selected.map(option => {
+  const userOptions = {};
+  options.selected.map((option) => {
     userOptions[camelize(String(option))] = true;
   });
   let css = '';
-  switch(baseCSS) {
-    case 'Eric Meyer CSS Reset (v2)':
-      css = getMeyerReset(userOptions);
+  switch (baseCSS) {
+    case 'Mini Reset':
+      css = getMiniReset(userOptions);
       break;
     case 'Normalize':
       css = getNormalize(userOptions);
       break;
-    case 'Mini Reset':
-      css = getMiniReset(userOptions);
+    default:
+      // 'Eric Meyer CSS Reset (v2)':
+      css = getMeyerReset(userOptions);
       break;
   }
   files.makeCSS(css);
-}
+};
 run();
